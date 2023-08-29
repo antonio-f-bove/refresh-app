cd80e24de3503d19fd45c0a89bdfa557b636ffcc
above <above@gualaclosures.com>
2023-08-29 21:27:21 +0200

    wip:Adds firebase sdk and setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'refresh-app-18ba8',
+    appId: '1:197752214301:web:6f8f1abe92c072597107f4',
+    storageBucket: 'refresh-app-18ba8.appspot.com',
+    apiKey: 'AIzaSyDkC_zFQMmtpU8v2AfZWYJQmyGkt9aEiRM',
+    authDomain: 'refresh-app-18ba8.firebaseapp.com',
+    messagingSenderId: '197752214301',
+    measurementId: 'G-T07JGPQNGP',
+  },
   production: false
 };
 
