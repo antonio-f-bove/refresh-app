17c11378ff653b19e9019d28249fc8fdcdf82e44
above <above@gualaclosures.com>
2023-08-29 21:36:25 +0200

    Fixes oversight in jest initial configuration

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  moduleNameMapper: {
+    '@core/(.*)': '<rootDir>/src/app/core/$1',
+  },
   preset: 'jest-preset-angular',
-  globalSetup: 'jest-preset-angular/global-setup',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/cypress/'],
 };
